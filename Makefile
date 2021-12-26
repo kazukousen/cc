da628c6f4b42db551abc221a5bff657d06c95a1e
kazukousen <mmchari.0228@gmail.com>
2021-12-26 22:53:33 +0900

    add + and - operators

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 cc: main.go
-	docker run --rm -w /tmp/cc -v $(shell pwd):/tmp/cc tinygo/tinygo:0.21.0 /bin/bash -c \
-		'tinygo build -o cc'
+	# docker run --rm -w /tmp/cc -v $(shell pwd):/tmp/cc tinygo/tinygo:0.21.0 /bin/bash -c 'tinygo build -o cc'
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o cc main.go
 
 test: cc
 	docker run --rm -v $(shell pwd):/cc -w /cc compilerbook bash -c \
--- main.go
+++ main.go
@@ -3,23 +3,43 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
+
+var in string
 
 func main() {
 	if len(os.Args) != 2 {
 		_, _ = fmt.Fprintf(os.Stderr, "the number of arguments is insufficient\n")
 		os.Exit(1)
 	}
-	n, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "the argument must be number\n")
-	}
+
+	in = os.Args[1]
 
 	fmt.Printf(`.intel_syntax noprefix
 .globl main
 main:
 	mov rax, %d
-	ret
-`, n)
+`, toInt())
+
+	for len(in) > 0 {
+		switch in[0] {
+		case '+':
+			in = in[1:]
+			fmt.Printf("	add rax, %d\n", toInt())
+		case '-':
+			in = in[1:]
+			fmt.Printf("	sub rax, %d\n", toInt())
+		}
+	}
+
+	fmt.Printf("	ret\n")
+}
+
+func toInt() int {
+	var ret int
+	for len(in) > 0 && in[0] >= '0' && in[0] <= '9' {
+		ret = ret*10 + int(in[0]-'0')
+		in = in[1:]
+	}
+	return ret
 }
