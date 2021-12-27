fb5e663c92a11822eb6f77293ed1d52f3ad62734
kazukousen <mmchari.0228@gmail.com>
2021-12-27 19:03:45 +0900

    refactor: add whitepace between operands in assembly

--- codegen.go
+++ codegen.go
@@ -13,7 +13,7 @@ func gen(n *node) {
 	case nodeKindLocal:
 		genLocal(n)
 		fmt.Printf("	pop rax\n")
-		fmt.Printf("	mov rax,[rax]\n")
+		fmt.Printf("	mov rax, [rax]\n")
 		fmt.Printf("	push rax\n")
 		return
 	case nodeKindAssign:
@@ -21,7 +21,7 @@ func gen(n *node) {
 		gen(n.rhs)
 		fmt.Printf("	pop rdi\n")
 		fmt.Printf("	pop rax\n")
-		fmt.Printf("	mov [rax],rdi\n")
+		fmt.Printf("	mov [rax], rdi\n")
 		fmt.Printf("	push rdi\n")
 		return
 	}
@@ -38,30 +38,30 @@ func gen(n *node) {
 
 	switch n.kind {
 	case nodeKindAdd:
-		fmt.Printf("	add rax,rdi\n")
+		fmt.Printf("	add rax, rdi\n")
 	case nodeKindSub:
-		fmt.Printf("	sub rax,rdi\n")
+		fmt.Printf("	sub rax, rdi\n")
 	case nodeKindMul:
-		fmt.Printf("	imul rax,rdi\n")
+		fmt.Printf("	imul rax, rdi\n")
 	case nodeKindDiv:
 		fmt.Printf("	cqo\n")
 		fmt.Printf("	idiv rdi\n")
 	case nodeKindLt:
-		fmt.Printf("	cmp rax,rdi\n")
+		fmt.Printf("	cmp rax, rdi\n")
 		fmt.Printf("	setl al\n")
-		fmt.Printf("	movzb rax,al\n")
+		fmt.Printf("	movzb rax, al\n")
 	case nodeKindLe:
-		fmt.Printf("	cmp rax,rdi\n")
+		fmt.Printf("	cmp rax, rdi\n")
 		fmt.Printf("	setle al\n")
-		fmt.Printf("	movzb rax,al\n")
+		fmt.Printf("	movzb rax, al\n")
 	case nodeKindEq:
-		fmt.Printf("	cmp rax,rdi\n")
+		fmt.Printf("	cmp rax, rdi\n")
 		fmt.Printf("	sete al\n")
-		fmt.Printf("	movzb rax,al\n")
+		fmt.Printf("	movzb rax, al\n")
 	case nodeKindNe:
-		fmt.Printf("	cmp rax,rdi\n")
+		fmt.Printf("	cmp rax, rdi\n")
 		fmt.Printf("	setne al\n")
-		fmt.Printf("	movzb rax,al\n")
+		fmt.Printf("	movzb rax, al\n")
 	}
 
 	fmt.Printf("	push rax\n")
@@ -72,7 +72,7 @@ func genLocal(n *node) {
 		_, _ = fmt.Fprintln(os.Stderr, "left side of assignment is not variable")
 		os.Exit(1)
 	}
-	fmt.Printf("	mov rax,rbp\n")
-	fmt.Printf("	sub rax,%d\n", n.offset)
+	fmt.Printf("	mov rax, rbp\n")
+	fmt.Printf("	sub rax, %d\n", n.offset)
 	fmt.Printf("	push rax\n")
 }
